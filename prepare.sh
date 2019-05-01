d7ca936ce6737456866ea6d9f27edc0503bfaa50
root <nisouris@hotmail.com>
2019-05-01 16:43:51 +0000

    source bashrc

--- prepare.sh
+++ prepare.sh
@@ -2,18 +2,18 @@
 
 set -o errexit
 set -o nounset
-set -o xtrace
+#set -o xtrace
 
-if [[ -x $(go version) ]]
+if [[ -z $(go version) ]]
 then
 	cd /tmp
 	wget https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh
 	chmod +x goinstall.sh
-	bash goinstall.sh --64
+	bash goinstall.sh --64 && source ~/.bashrc
 fi
 cd ~
 
-if [[ -x $(git version) ]]
+if [[ -z $(git version) ]]
 then
 	apt install -y git
 fi
